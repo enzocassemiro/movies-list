74334f6cf0a5d2dea007f6871913c9a95c87fcba
enzocass <enzocass@live.com>
2022-08-17 16:42:42 -0300

    refactor(movie-service): remove movie models

--- src/app/core/services/api/movie-db.service.ts
+++ src/app/core/services/api/movie-db.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MoviePopularPages } from '../../models/movie-popular.model';
-import { Movie } from '../../models/movie.models';
 
 
 @Injectable({
