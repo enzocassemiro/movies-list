f0b475d68a0e31a239346d61d3469a9f69143ba4
enzocass <enzocass@live.com>
2022-08-24 17:07:40 -0300

    refactor(ListService): change model name

--- src/app/core/services/api/movie-db.service.ts
+++ src/app/core/services/api/movie-db.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { MoviePopularPages } from '../../models/movie-popular.model';
+import { MovieCategoryPages } from '../../models/movie-category.model';
 
 
 @Injectable({
@@ -15,7 +15,7 @@ export class MovieDbService {
   private apiURL = 'https://api.themoviedb.org/3/';
   private apiKEY = '4a15c71ea03c69929b12a7476575d76c';
 
-  getAllMovieByCategory(category: string, page: number, language: string): Observable<MoviePopularPages> {
-    return this.http.get<MoviePopularPages>(`${this.apiURL}movie/${category}?api_key=${this.apiKEY}&language=${language}&page=${page}`)
+  getAllMovieByCategory(category: string, page: number, language: string): Observable<MovieCategoryPages> {
+    return this.http.get<MovieCategoryPages>(`${this.apiURL}movie/${category}?api_key=${this.apiKEY}&language=${language}&page=${page}`)
   }
 }
